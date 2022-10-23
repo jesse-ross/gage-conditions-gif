cb3023a82cce3c55ce2f547dd9619f39464b2895
Jesse Ross <jross@usgs.gov>
2022-10-23 14:10:03 -0700

    fix path in shell script

--- aws/run-pipeline.sh
+++ aws/run-pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Rscript -e 'scipiper::scmake("2_process/out/dv_stats.rds")'
-Rscript -e 'readr::write_csv(readRDS("2_process/out/dv_stats.rds"), "/output/dv_stats.rds")'
+Rscript -e 'setwd("/code"); scipiper::scmake("2_process/out/dv_stats.rds")'
+Rscript -e 'readr::write_csv(readRDS("/code/2_process/out/dv_stats.rds"), "/output/dv_stats.rds")'
