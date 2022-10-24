36eb34bd6b7cd345175d2e19e526cf2ffc227da5
Jesse Ross <jross@usgs.gov>
2022-10-24 14:03:52 -0700

    use customized version of scipiper

--- aws/run-pipeline.sh
+++ aws/run-pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-Rscript -e 'setwd("/code"); scipiper::scmake("2_process/out/dv_stats.rds")'
+export AWS_DEFAULT_REGION=us-west-2
+Rscript -e 'setwd("/code"); cat("cd-ed\n"); cat(paste("region is", Sys.getenv("AWS_DEFAULT_REGION"), "\n")); scipiper::scmake("2_process/out/dv_stats.rds"); cat("ran stats\n")'
 Rscript -e 'readr::write_csv(readRDS("/code/2_process/out/dv_stats.rds"), "/output/dv_stats.rds")'
