4383f67f852d7c190de5ec06ee6e0c6897b7a5fb
Jesse Ross <jross@usgs.gov>
2022-10-26 10:24:10 -0700

    improve naming for ephemeral volume

--- aws/run-pipeline.sh
+++ aws/run-pipeline.sh
@@ -3,4 +3,6 @@
 export AWS_DEFAULT_REGION=us-west-2
 cd /code/
 Rscript -e 'cat(paste("region is", Sys.getenv("AWS_DEFAULT_REGION"), "\n")); cat(paste("scipiper.use_local_aws_credentials is", options("scipiper.use_local_aws_credentials"), "\n")); scipiper::scmake("2_process/out/dv_stats.rds"); cat("ran stats\n")'
-Rscript -e 'cat("REFORMATTING dv_stats file\n"); cat("READING 2_process/out/dv_stats.rds\n"); readr::write_csv(readRDS("2_process/out/dv_stats.rds"), "/output/dv_stats.csv"); cat("WROTE /output/dv_stats.csv\n")'
+Rscript -e 'cat("REFORMATTING dv_stats file\n"); cat("READING 2_process/out/dv_stats.rds\n"); readr::write_csv(readRDS("2_process/out/dv_stats.rds"), "/ephemeral/dv_stats.csv"); cat("WROTE /ephemeral/dv_stats.csv\n")'
+cp 2_process/out/dv_stats.rds /ephemeral/
+echo "COPIED dv_stats.rds to /ephemeral/"
