41677e285600770e2e43282cee7fec9ec2fffa00
Jesse Ross <jross@usgs.gov>
2022-12-20 08:05:29 -0800

    use arrows for assignment

--- 1_fetch/src/calculate_previous_month_boundaries.R
+++ 1_fetch/src/calculate_previous_month_boundaries.R
@@ -1,10 +1,10 @@
 #' @title Calculate the beginning and end of the previous month
 #'
 calculate_previous_month_boundaries <- function(){
-  current_date = Sys.Date()
-  month_end = current_date - days(day(current_date))
-  previous_month = list()
-  previous_month[['start']] = month_end - days(day(month_end)) + 1
-  previous_month[['end']] = month_end
+  current_date <- Sys.Date()
+  month_end <- current_date - days(day(current_date))
+  previous_month <- list()
+  previous_month[['start']] <- month_end - days(day(month_end)) + 1
+  previous_month[['end']] <- month_end
   previous_month
 }
